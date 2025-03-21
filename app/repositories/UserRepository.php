bb2ca81d84ef149c899b2cf7750ad127189e4adc
anouar el barry <elbarryanwar37@gmail.com>
2025-03-21 11:49:29 +0000

    create user repository

---
+++ app/repositories/UserRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Repositories;
+use App\Models\User;
+class UserRepository{
+    protected $userModel;
+    public function __construct(User $userModel){
+        $this->userModel = $userModel;
+    }
+
+    public function findbyusername($username){
+       return $this->userModel->where('name',$username)->first();
+    }
+    public function isEmailUnique($email){
+       return $this->userModel->where('email',$email)->doesntExist();
+    }
+
+    public function createUser(array $userData){
+        return $this->userModel->create($userData);
+    }
+
+}
