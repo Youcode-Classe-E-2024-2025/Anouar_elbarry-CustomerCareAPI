11b87fddc5a41338d25cc35201c7968416afe62b
anouar el barry <elbarryanwar37@gmail.com>
2025-03-21 12:10:12 +0000

    create UserService with needed methods

---
+++ app/services/UserService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\services;
+use App\Repositories\UserRepository;
+use Hash;
+use Illuminate\Support\Facades\Auth;
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository){
+      $this->userRepository = $userRepository;
+    }
+    public function register(array $userData)
+    {
+        if($this->userRepository->isEmailUnique($userData['email'])){
+           $userData['password'] = bcrypt($userData['password']);
+           $user = $this->userRepository->createUser($userData);
+           return $user;
+        }
+        return false;
+    }
+
+    public function login(array $userData)
+    {    
+        $user = $this->userRepository->findUserByEmail($userData['email']);
+        if($user && Hash::check($userData['password'],$user->password)){
+            Auth::login($user);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function logout()
+    {
+         Auth::logout();
+         return true;
+    }
+}
