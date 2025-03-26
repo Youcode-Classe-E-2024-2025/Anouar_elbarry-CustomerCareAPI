ff5aa4abbaf24b7e5a52dee530733689e4f9382a
anouar el barry <elbarryanwar37@gmail.com>
2025-03-26 22:20:45 +0000

    add routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,12 +1,36 @@
-import { createRouter, createWebHistory  } from! "vue-router";
-import ExampleComponent from "./components/ExampleComponent.vue";
+import { createRouter, createWebHistory  } from "vue-router";
+import HomeComponent from "./components/home.vue";
+import NavComponent from "./components/nav.vue";
+import FooterComponent from "./components/footer.vue";
+import tickets from "./pages/tickets.vue";
+import Auth from "./pages/auth.vue";
 
 const routes = [
     {
         path : '/',
         name : 'home',
-        component : ExampleComponent
-    },
+        components : {
+            default : HomeComponent,
+            navbar : NavComponent,
+            footer : FooterComponent
+        }
+    },{
+        path : '/tickets',
+        name : 'tickets',
+        components : {
+            default : tickets,
+            navbar : NavComponent,
+            footer : FooterComponent
+        }
+    },{
+        path : '/auth',
+        name : 'auth',
+        components : {
+            default : Auth,
+            navbar : NavComponent,
+            footer : FooterComponent
+        }
+    }
 ]
 
 const router = createRouter({
