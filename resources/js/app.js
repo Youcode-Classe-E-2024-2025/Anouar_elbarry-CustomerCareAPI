14251c4a54a2f1fa7d1aeda1d91ebbc7248c7c65
anouar el barry <elbarryanwar37@gmail.com>
2025-03-26 11:16:27 +0000

    add other components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,18 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+
 
 import './bootstrap';
 import { createApp } from 'vue';
+import router from './router';
 
-/**
- * Next, we will create a fresh Vue application instance. You may then begin
- * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
- */
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+import home from './components/home.vue';
+import tickets from './pages/tickets.vue';
+import nav from './components/nav.vue';
+app.component('home', home);
+app.component('tickets', tickets);
+app.component('nav-component',nav);
 
+app.use(router);
 app.mount('#app');
