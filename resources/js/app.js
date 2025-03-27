316bfd491728cd967863f88b763b3f3f57aa1c83
anouar el barry <elbarryanwar37@gmail.com>
2025-03-27 10:42:03 +0000

    organize the code

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,18 @@ import router from './router';
 
 const app = createApp({});
 
+import auth from './pages/auth.vue'
+app.component('auth',auth);
+
 import home from './components/home.vue';
-import tickets from './pages/tickets.vue';
-import nav from './components/nav.vue';
 app.component('home', home);
+
+import tickets from './pages/tickets.vue';
 app.component('tickets', tickets);
+
+import nav from './components/nav.vue';
 app.component('nav-component',nav);
+
 
 app.use(router);
 app.mount('#app');
